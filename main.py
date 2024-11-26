f1ac4539868439eede31d2d9ed10cfe01713678b
Temulen1225 <temulen1225@gmail.com>
2024-11-26 05:24:53 +0000

    g

--- main.py
+++ main.py
@@ -12,26 +12,30 @@ def index():
         <form action="/hello" method="GET">
           名前: <input type="text" name="name">
           <input type="submit" value="送信">
+          ひとこと: <input type="text" comment="comment">
+            <input type="submit" value="送信">
         </form>
         </body></html>
     """
 
+
 # /hello へアクセスがあった時 --- (*3)
 @app.route('/hello')
 def hello():
-        # nameのパラメータを得る --- (*4)
-        name = request.args.get('name')
-        comment = request.args.get('comment')
-        if name is None:
-            name = '名無し'
-        if comment is None:
-            comment = 'コメント無し'
-    # 自己紹介を自動作成
-        return """
+    # nameのパラメータを得る --- (*4)
+    name = request.args.get('name')
+    comment = request.args.get('comment')
+    if name is None:
+        name = '名無し'
+    if comment is None:
+        comment = 'コメント無し'
+
+
+# 自己紹介を自動作成
+    return """
         <h1>{0}さん、こんにちは！</h1>
         ひとこと：{1}
         """.format(name, comment)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
